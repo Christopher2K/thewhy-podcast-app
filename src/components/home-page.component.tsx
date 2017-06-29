737d274f02161abeb814d1f19e1bd2704ae68d55
Christopher N. KATOYI-KABA <christopher.katoyi@icloud.com>
2017-06-30 00:39:54 +0200

    Webpack config

--- src/components/home-page.component.tsx
+++ src/components/home-page.component.tsx
@@ -9,13 +9,15 @@ const whyLogo = require('../assets/img/the-why-inline.png');
 export const Home: React.SFC = (): JSX.Element => (
     <div className="home page">
         <div className="home__header">
-            <img className="header__logo" src={whyLogo} alt="The Why Le Poukwa Logo"/>
+            <img className="header__logo" src={whyLogo} alt="The Why Le Poukwa Logo" />
             <h2 className="header__tag-line">Slogan de la mort qui tue sa mère la ****</h2>
         </div>
 
         <div className="home__buttons">
-            <Link to={`/concept`}>Concept</Link>
-            <Link to={`/podcasts`}>Podcasts</Link>
+            <button className="button">
+                <Link to={`/concept`}>Concept</Link>
+                <Link to={`/podcasts`}>Podcasts</Link>
+            </button>
         </div>
 
         <Socials />
